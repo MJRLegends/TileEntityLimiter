ad0366ca4db525e6dc7ed9b477fc40012bf429ae
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-13 23:37:36 +0000

    Backport to MC 1.7.10

--- src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
+++ src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
@@ -1,16 +1,16 @@
 package com.mjr.tileentitylimiter;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:forge@[14.23.1.2555,);",serverSideOnly = true)
+@Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:Forge@[10.13.4.1558,);", acceptableRemoteVersions="*")
 public class TileEntityLimiter {
 	public static final String MODID = "tileentitylimiter";
-	public static final String VERSION = "1.12.2-1.0.0";
+	public static final String VERSION = "1.7.10-1.0.0";
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {

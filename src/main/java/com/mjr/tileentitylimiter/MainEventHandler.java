b6eb7e3a9c6f75d311f2fa7158b88683539bf3bd
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-13 23:37:59 +0000

    Backport to MC 1.10.2

--- src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
+++ src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
@@ -11,11 +11,11 @@ public class MainEventHandler {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onBlockPlacement(BlockEvent.PlaceEvent event) {
 		if (Config.enablePerChunkLimit && event.getWorld().getChunkFromBlockCoords(event.getPos()).getTileEntityMap().size() > Config.perChunkLimit) {
-			event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
+			event.getPlayer().addChatMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
 			event.setCanceled(true);
 		}
 		if (Config.enablePerChunkWorld && event.getWorld().loadedTileEntityList.size() > Config.perChunkWorld) {
-			event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
+			event.getPlayer().addChatMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
 			event.setCanceled(true);
 		}
 	}

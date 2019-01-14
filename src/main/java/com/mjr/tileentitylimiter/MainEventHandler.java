6f84a43c6742bec77806e7dcc21a3274e8763757
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-14 01:17:47 +0000

    Backport to MC 1.8.9

--- src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
+++ src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
@@ -1,7 +1,7 @@
 package com.mjr.tileentitylimiter;
 
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -10,12 +10,12 @@ public class MainEventHandler {
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onBlockPlacement(BlockEvent.PlaceEvent event) {
-		if (Config.enablePerChunkLimit && event.getWorld().getChunkFromBlockCoords(event.getPos()).getTileEntityMap().size() > Config.perChunkLimit) {
-			event.getPlayer().addChatMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
+		if (Config.enablePerChunkLimit && event.world.getChunkFromBlockCoords(event.pos).getTileEntityMap().size() > Config.perChunkLimit) {
+			event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
 			event.setCanceled(true);
 		}
-		if (Config.enablePerChunkWorld && event.getWorld().loadedTileEntityList.size() > Config.perChunkWorld) {
-			event.getPlayer().addChatMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
+		if (Config.enablePerChunkWorld && event.world.loadedTileEntityList.size() > Config.perChunkWorld) {
+			event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
 			event.setCanceled(true);
 		}
 	}

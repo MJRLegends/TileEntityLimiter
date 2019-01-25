a04cabfb2f8df0c7b2f318af0f3167255516db59
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-25 23:05:42 +0000

    Released v1.0.1, Fixed issue #2

--- src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
+++ src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
@@ -7,10 +7,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:Forge@[11.15.1.1764,);",serverSideOnly = true)
+@Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:Forge@[11.15.1.1764,);",serverSideOnly = true, acceptableRemoteVersions = "*")
 public class TileEntityLimiter {
 	public static final String MODID = "tileentitylimiter";
-	public static final String VERSION = "1.8.9-1.0.0";
+	public static final String VERSION = "1.8.9-1.0.1";
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {

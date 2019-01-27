b8ca52a3917ac7add59338db01fbc4a1e975749c
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-27 14:41:55 +0000

    Fixed issue #3, Added suggestion of a blacklist from limit check (#4)

--- src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
+++ src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
@@ -1,5 +1,7 @@
 package com.mjr.tileentitylimiter;
 
+import java.util.Arrays;
+
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.world.BlockEvent;
@@ -10,13 +12,25 @@ public class MainEventHandler {
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onBlockPlacement(BlockEvent.PlaceEvent event) {
-		if (Config.enablePerChunkLimit && event.getWorld().getChunkFromBlockCoords(event.getPos()).getTileEntityMap().size() > Config.perChunkLimit) {
-			event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
-			event.setCanceled(true);
+		if (Config.enablePerChunkLimit) {
+			if (event.getBlockSnapshot().getTileEntity() != null) {
+				if (!Arrays.asList(Config.tileEntityBlockList).contains(event.getBlockSnapshot().getRegistryName().toString())) {
+					if (event.getWorld().getChunkFromBlockCoords(event.getPos()).getTileEntityMap().size() > Config.perChunkLimit) {
+						event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
+						event.setCanceled(true);
+					}
+				}
+			}
 		}
-		if (Config.enablePerChunkWorld && event.getWorld().loadedTileEntityList.size() > Config.perChunkWorld) {
-			event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
-			event.setCanceled(true);
+		if (Config.enablePerChunkWorld) {
+			if (event.getBlockSnapshot().getTileEntity() != null) {
+				if (!Arrays.asList(Config.tileEntityBlockList).contains(event.getBlockSnapshot().getRegistryName().toString())) {
+					if (event.getWorld().loadedTileEntityList.size() > Config.perChunkWorld) {
+						event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
+						event.setCanceled(true);
+					}
+				}
+			}
 		}
 	}
 }

73b5463046158123ffb1d6c6edf435bc78f05d19
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-27 14:44:57 +0000

    Released v1.0.2

--- src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
+++ src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:forge@[14.23.1.2555,);", serverSideOnly = true, acceptableRemoteVersions = "*", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class TileEntityLimiter {
 	public static final String MODID = "tileentitylimiter";
-	public static final String VERSION = "1.12.2-1.0.1";
+	public static final String VERSION = "1.12.2-1.0.2";
 	public static Logger logger;
 
 	@Mod.EventHandler

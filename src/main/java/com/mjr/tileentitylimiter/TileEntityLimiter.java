bbd90afe1aefa70f76b8f8768fa08137cce17200
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-27 14:42:24 +0000

    Added jar signing fingerprint check

--- src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
+++ src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
@@ -1,30 +1,39 @@
 package com.mjr.tileentitylimiter;
 
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:forge@[14.23.1.2555,);",serverSideOnly = true, acceptableRemoteVersions = "*")
+@Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:forge@[14.23.1.2555,);", serverSideOnly = true, acceptableRemoteVersions = "*", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class TileEntityLimiter {
 	public static final String MODID = "tileentitylimiter";
 	public static final String VERSION = "1.12.2-1.0.1";
+	public static Logger logger;
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
+		logger = event.getModLog();
 		Config.load();
 		MinecraftForge.EVENT_BUS.register(new MainEventHandler());
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-
 	}
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
 
+	}
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+		logger.fatal("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported!");
 	}
 }

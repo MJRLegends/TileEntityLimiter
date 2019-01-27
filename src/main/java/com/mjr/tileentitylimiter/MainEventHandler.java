aab3a460d3149131a167eed524a8d589b6bf6e8f
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-27 14:41:55 +0000

    Fixed issue #3, Added suggestion of a blacklist from limit check (#4)

--- src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
+++ src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
@@ -1,5 +1,7 @@
 package com.mjr.tileentitylimiter;
 
+import java.util.Arrays;
+
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.world.BlockEvent;
@@ -10,13 +12,25 @@ public class MainEventHandler {
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onBlockPlacement(BlockEvent.PlaceEvent event) {
-		if (Config.enablePerChunkLimit && event.world.getChunkFromBlockCoords(event.pos).getTileEntityMap().size() > Config.perChunkLimit) {
-			event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
-			event.setCanceled(true);
+		if (Config.enablePerChunkLimit) {
+			if (event.blockSnapshot.getTileEntity() != null) {
+				if (!Arrays.asList(Config.tileEntityBlockList).contains(event.blockSnapshot.blockIdentifier.toString())) {
+					if (event.world.getChunkFromBlockCoords(event.pos).getTileEntityMap().size() > Config.perChunkLimit) {
+						event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
+						event.setCanceled(true);
+					}
+				}
+			}
 		}
-		if (Config.enablePerChunkWorld && event.world.loadedTileEntityList.size() > Config.perChunkWorld) {
-			event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
-			event.setCanceled(true);
+		if (Config.enablePerChunkWorld) {
+			if (event.blockSnapshot.getTileEntity() != null) {
+				if (!Arrays.asList(Config.tileEntityBlockList).contains(event.blockSnapshot.blockIdentifier.toString())) {
+					if (event.world.loadedTileEntityList.size() > Config.perChunkWorld) {
+						event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
+						event.setCanceled(true);
+					}
+				}
+			}
 		}
 	}
 }

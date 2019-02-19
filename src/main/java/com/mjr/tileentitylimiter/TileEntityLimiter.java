7c415067cf44e099533c57924e6377b54593b64a
MJRLegends <MJRLegends@users.noreply.github.com>
2019-02-19 20:12:17 +0000

    Added back jar signing, but no output will be fired yet when its missing. Due to FMLFingerprintViolationEvent isnt being fired yet

--- src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
+++ src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
@@ -1,25 +1,30 @@
 package com.mjr.tileentitylimiter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("tileentitylimiter")
 public class TileEntityLimiter {
+	private static final Logger logger = LogManager.getLogger();
 
-    public TileEntityLimiter() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.serverSpec);
-    }
-    
-    private void init(final FMLCommonSetupEvent event) {
-       MinecraftForge.EVENT_BUS.register(new MainEventHandler());
-    }
-    
-//	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
-//		logger.fatal("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported!");
-//	}
+	public TileEntityLimiter() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.serverSpec);
+	}
+
+	private void init(final FMLCommonSetupEvent event) {
+		MinecraftForge.EVENT_BUS.register(new MainEventHandler());
+	}
+
+	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+		logger.fatal("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported!");
+	}
 }

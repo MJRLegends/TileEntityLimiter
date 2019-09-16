f8e1a6c31da637ae7c2bf90753653b5ef8a2393d
MJRLegends <MJRLegends@users.noreply.github.com>
2019-09-16 23:41:29 +0100

    Update to the latest forge version

--- src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
+++ src/main/java/com/mjr/tileentitylimiter/MainEventHandler.java
@@ -2,19 +2,19 @@ package com.mjr.tileentitylimiter;
 
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.BlockEvent.EntityPlaceEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class MainEventHandler {
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
-	public void onBlockPlacement(BlockEvent.PlaceEvent event) {
+	public void onBlockPlacement(EntityPlaceEvent event) {
 		if (Config.SERVER.enablePerChunkLimit.get()) {
 			if (event.getBlockSnapshot().getTileEntity() != null) {
 				if (!Config.SERVER.tileEntityBlockList.get().contains(event.getBlockSnapshot().getRegistryName().toString())) {
 					if (event.getWorld().getWorld().getChunk(event.getPos()).getTileEntityMap().size() > Config.SERVER.perChunkLimit.get()) {
-						event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
+						event.getEntity().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per chunk has been reached!"));
 						event.setCanceled(true);
 					}
 				}
@@ -24,7 +24,7 @@ public class MainEventHandler {
 			if (event.getBlockSnapshot().getTileEntity() != null) {
 				if (!Config.SERVER.tileEntityBlockList.get().contains(event.getBlockSnapshot().getRegistryName().toString())) {
 					if (event.getWorld().getWorld().loadedTileEntityList.size() > Config.SERVER.perChunkWorld.get()) {
-						event.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
+						event.getEntity().sendMessage(new TextComponentString(TextFormatting.RED + "Sorry, Tile entity limit per world has been reached!"));
 						event.setCanceled(true);
 					}
 				}

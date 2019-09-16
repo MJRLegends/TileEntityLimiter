000306698c6baa6282aed57d6ea8d0f4a61d3d22
MJRLegends <MJRLegends@users.noreply.github.com>
2019-09-17 00:40:44 +0100

    Fixed event not working, Released v1.0.2

--- src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
+++ src/main/java/com/mjr/tileentitylimiter/TileEntityLimiter.java
@@ -10,7 +10,7 @@ import net.minecraftforge.common.MinecraftForge;
 @Mod(modid = TileEntityLimiter.MODID, version = TileEntityLimiter.VERSION, dependencies = "required-after:Forge@[10.13.4.1558,);", acceptableRemoteVersions="*")
 public class TileEntityLimiter {
 	public static final String MODID = "tileentitylimiter";
-	public static final String VERSION = "1.7.10-1.0.1";
+	public static final String VERSION = "1.7.10-1.0.2";
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
